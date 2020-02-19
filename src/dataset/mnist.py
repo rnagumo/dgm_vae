6507a6ed89eb6206c88d8cecb37af2344f117c2f
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-02-19 21:01:24 +0900

    [update] image size to  (batch_size, channels, height, width)

--- src/dataset/mnist.py
+++ src/dataset/mnist.py
@@ -10,18 +10,15 @@ def init_mnist_dataloader(root, batch_size, cuda=False):
     # Kwargs for data loader
     kwargs = {"num_workers": 1, "pin_memory": True} if cuda else {}
 
-    # Transform
-    transform = transforms.Compose([
-        transforms.ToTensor(), transforms.Lambda(lambd=lambda x: x.view(-1))])
-
     # Instantiate data loader
     train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST(root=root, train=True, transform=transform,
+        datasets.MNIST(root=root, train=True, transform=transforms.ToTensor(),
                        download=True),
         shuffle=True, **kwargs)
 
     test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST(root=root, train=False, transform=transform),
-        shuffle=True, **kwargs)
+        datasets.MNIST(root=root, train=False,
+                       transform=transforms.ToTensor()),
+        shuffle=False, **kwargs)
 
     return train_loader, test_loader
