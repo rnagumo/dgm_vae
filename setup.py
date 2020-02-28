3e696a95a767f23a033ed256c6e509009c2ea4a6
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-02-28 16:44:50 +0900

    [add] setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    "torch>=1.0",
+    "torchvision",
+    "pixyz>=0.1.3",
+]
+
+setup(
+    name="dgm_vae",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=install_requires,
+)
