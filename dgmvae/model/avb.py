59607529cd4ded774843f57009a0c8f5dc34acf2
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-03-04 20:22:45 +0900

    [update] avb model

--- dgmvae/model/avb.py
+++ dgmvae/model/avb.py
@@ -4,12 +4,18 @@
 Adversarial Variational Bayes: Unifying Variational Autoencoders and Generative
 Adversarial Networks
 http://arxiv.org/abs/1701.04722
+
+Reference
+https://github.com/gdikov/adversarial-variational-bayes
+http://seiya-kumada.blogspot.com/2018/07/adversarial-variational-bayes.html
+https://github.com/LMescheder/AdversarialVariationalBayes
 """
 
 import collections
 
 import torch
-from torch import nn
+from torch import nn, optim
+from torch.nn import functional as F
 
 import pixyz.distributions as pxd
 import pixyz.losses as pxl
@@ -17,40 +23,117 @@ import pixyz.losses as pxl
 from .base import BaseVAE
 
 
-class Discriminator(pxd.Deterministic):
-    def __init__(self, z_dim):
-        super().__init__(cond_var=["z"], var=["t"])
+class AVBDiscriminator(pxd.Deterministic):
+    """T(x, z)"""
+    def __init__(self, channel_num, z_dim):
+        super().__init__(cond_var=["x", "z"], var=["t"])
 
-        self.model = nn.Sequential(
-            nn.Linear(z_dim, 1000),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(1000, 1000),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(1000, 1000),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(1000, 1000),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(1000, 1000),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(1000, 1000),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(1000, 1),
+        self.x_disc = nn.Sequential(
+            nn.Conv2d(channel_num, 32, 4, stride=2, padding=1),
+            nn.Conv2d(32, 32, 4, stride=2, padding=1),
+            nn.Conv2d(32, 64, 4, stride=2, padding=1),
+            nn.Conv2d(64, 64, 4, stride=2, padding=1),
+        )
+        self.fc_x = nn.Linear(1024, 256)
+
+        self.z_disc = nn.Sequential(
+            nn.Linear(z_dim, 512),
+            nn.Linear(512, 512),
+            nn.Linear(512, 256),
         )
 
-    def forward(self, z):
-        logits = self.model(z)
+        self.fc = nn.Linear(512, 1)
+
+    def forward(self, x, z):
+        h_x = self.x_disc(x)
+        h_x = self.fc_x(h_x.view(-1, 1024))
+        h_z = self.z_disc(z)
+        logits = self.fc(torch.cat([h_x, h_z], dim=1))
         probs = torch.sigmoid(logits)
         t = torch.clamp(probs, 1e-6, 1 - 1e-6)
         return {"t": t}
 
 
-class AVB(BaseVAE):
-    def __init__(self, channel_num, z_dim, device, beta, **kwargs):
-        super().__init__(channel_num, z_dim, device, beta, **kwargs)
+class AVBEncoder(pxd.Deterministic):
+    """Deterministic encoder z_phi (x, e)"""
+    def __init__(self, channel_num, z_dim, e_dim):
+        super().__init__(cond_var=["x", "e"], var=["z"])
 
-        self.disc = Discriminator(z_dim).to(device)
+        self.enc_x = nn.Sequential(
+            nn.Conv2d(channel_num, 32, 4, stride=2, padding=1),
+            nn.Conv2d(32, 32, 4, stride=2, padding=1),
+            nn.Conv2d(32, 64, 4, stride=2, padding=1),
+            nn.Conv2d(64, 64, 4, stride=2, padding=1),
+        )
+        self.fc_x = nn.Sequential(
+            nn.Linear(1024, 256),
+            nn.Linear(256, z_dim)
+        )
+        self.fc_e = nn.Linear(e_dim, z_dim)
+        self.fc = nn.Linear(z_dim * 2, z_dim)
+
+    def forward(self, x, e):
+        h_x = self.enc_x(x)
+        h_x = h_x.view(-1, 1024)
+        h_x = F.relu(self.fc_x(h_x))
+        h_e = self.fc_e(e)
+        z = self.fc(torch.cat([h_x, h_e], dim=1))
+        return {"z": z}
+
+
+class Decoder(pxd.Bernoulli):
+    def __init__(self, channel_num, z_dim):
+        super().__init__(cond_var=["z"], var=["x"])
+
+        self.fc1 = nn.Linear(z_dim, 256)
+        self.fc2 = nn.Linear(256, 1024)
+        self.deconv1 = nn.ConvTranspose2d(64, 64, 4, stride=2, padding=1)
+        self.deconv2 = nn.ConvTranspose2d(64, 32, 4, stride=2, padding=1)
+        self.deconv3 = nn.ConvTranspose2d(32, 32, 4, stride=2, padding=1)
+        self.deconv4 = nn.ConvTranspose2d(32, channel_num, 4, stride=2,
+                                          padding=1)
+
+    def forward(self, z):
+        h = F.relu(self.fc1(z))
+        h = F.relu(self.fc2(h))
+        h = h.view(-1, 64, 4, 4)
+        h = F.relu(self.deconv1(h))
+        h = F.relu(self.deconv2(h))
+        h = F.relu(self.deconv3(h))
+        probs = torch.sigmoid(self.deconv4(h))
+        return {"probs": probs}
+
+
+class AVB(BaseVAE):
+    def __init__(self, channel_num, z_dim, e_dim, device, beta, **kwargs):
+
+        self.channel_num = channel_num
+        self.z_dim = z_dim
+        self.e_dim = e_dim
+        self.device = device
+        self.beta = beta
+
+        # Distributions
+        self.normal = pxd.Normal(loc=torch.tensor(0.), scale=torch.tensor(1.),
+                                 var=["e"], features_shape=[e_dim]).to(device)
+        self.prior = pxd.Normal(loc=torch.tensor(0.), scale=torch.tensor(1.),
+                                var=["z"], features_shape=[z_dim]).to(device)
+        self.decoder = Decoder(channel_num, z_dim).to(device)
+        self.encoder = AVBEncoder(channel_num, z_dim, e_dim).to(device)
+        self.distributions = nn.ModuleList(
+            [self.normal, self.prior, self.decoder, self.encoder])
+
+        # Loss
+        self.ce = pxl.CrossEntropy(self.encoder, self.decoder)
+
+        # Adversarial loss
+        self.disc = AVBDiscriminator(channel_num, z_dim).to(device)
         self.adv_js = pxl.AdversarialJensenShannon(
             self.encoder, self.prior, self.disc)
+
+        # Optimizer
+        params = self.distributions.parameters()
+        self.optimizer = optim.Adam(params)
 
     def _eval_loss(self, x_dict, **kwargs):
 
@@ -78,6 +161,9 @@ class AVB(BaseVAE):
 
             # Input
             x_dict = {"x": x}
+
+            # Sample e (noize)
+            x_dict = self.normal.sample(x_dict, batch_n=minibatch_size)
 
             # Calculate loss
             if training:
