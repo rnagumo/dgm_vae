384d1da4bb09af76c0354461ce638b5b3fd3f225
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-03-06 16:39:24 +0900

    Update main function

--- src/train.py
+++ src/train.py
@@ -3,148 +3,92 @@
 
 import argparse
 import datetime
+import json
 import pathlib
 
 import torch
-import tensorboardX as tb
+import pytorch_lightning as pl
 
-import dgmvae.dataset as dvd
 import dgmvae.model as dvm
-import dgmvae.utils as dvu
+import dgmvae.updaters as dvu
 
 
-def train(args, logger, config):
+def main():
 
     # -------------------------------------------------------------------------
     # 1. Settings
     # -------------------------------------------------------------------------
 
+    # Kwargs
+    args = init_args()
+
+    # Configs
+    with pathlib.Path(args.config).open() as f:
+        config = json.load(f)
+
     # Cuda setting
-    use_cuda = torch.cuda.is_available()
-    device = torch.device(f"cuda:{args.cuda_num}" if use_cuda else "cpu")
-    logger.info(f"Device: {device}")
+    use_cuda = torch.cuda.is_available() and args.cuda is not None
+    gpus = args.cuda if use_cuda else None
 
     # Random seed
     torch.manual_seed(args.seed)
 
     # Tensorboard writer
-    writer = tb.SummaryWriter(args.logdir)
+    # logger = pl.loggers.TensorBoardLogger(args.logdir)
 
     # Timer
     timer = datetime.datetime.now()
 
     # -------------------------------------------------------------------------
-    # 2. Data
+    # 2. Training
     # -------------------------------------------------------------------------
 
-    # Loader
-    train_loader, test_loader = dvd.init_mnist_dataloader(
-        args.root, args.batch_size, use_cuda)
+    # VAE model
+    model_params = {"channel_num": 1}
 
-    # Data dimension, (batch_size, channel_num, height, width)
-    channel_num = iter(train_loader).next()[0].size(1)
-
-    # Sample data for comparison
-    x_org, _ = iter(test_loader).next()
-
-    # Log
-    logger.info(f"Train data size: {train_loader.dataset.data.size()}")
-    logger.info(f"Test data size: {test_loader.dataset.data.size()}")
-
-    # -------------------------------------------------------------------------
-    # 3. Model
-    # -------------------------------------------------------------------------
-
-    params = {"channel_num": channel_num, "device": device}
-
-    if args.model == "vae":
-        model = dvm.BaseVAE(**config["vae_params"], **params)
-    elif args.model == "beta":
-        model = dvm.BetaVAE(**config["beta_params"], **params)
+    if args.model == "beta":
+        model = dvm.BetaVAE(**config["beta_params"], **model_params)
     elif args.model == "factor":
-        model = dvm.FactorVAE(**config["factor_params"], **params)
+        model = dvm.FactorVAE(**config["factor_params"], **model_params)
     elif args.model == "dip-i":
-        model = dvm.DIPVAE(**config["dip-i_params"], **params)
+        model = dvm.DIPVAE(**config["dip-i_params"], **model_params)
     elif args.model == "dip-ii":
-        model = dvm.DIPVAE(**config["dip-ii_params"], **params)
+        model = dvm.DIPVAE(**config["dip-ii_params"], **model_params)
     elif args.model == "joint":
-        model = dvm.JointVAE(**config["joint_params"], **params)
+        model = dvm.JointVAE(**config["joint_params"], **model_params)
     elif args.model == "tcvae":
-        model = dvm.TCVAE(**config["tcvae_params"], **params)
+        model = dvm.TCVAE(**config["tcvae_params"], **model_params)
     elif args.model == "aae":
-        model = dvm.AAE(**config["aae_params"], **params)
+        model = dvm.AAE(**config["aae_params"], **model_params)
     elif args.model == "avb":
-        model = dvm.AVB(**config["avb_params"], **params)
+        model = dvm.AVB(**config["avb_params"], **model_params)
     else:
         raise KeyError(f"Not implemented model is specified, {args.model}")
 
-    # -------------------------------------------------------------------------
-    # 4. Training
-    # -------------------------------------------------------------------------
+    # Updater
+    updater_params = vars(args)
+    updater_params.update({"timer": timer})
+    updater = dvu.VAEUpdater(model, updater_params)
 
-    for epoch in range(1, args.epochs + 1):
-        logger.info(f"--- Epoch {epoch} ---")
-
-        # Training
-        train_loss = model.run(train_loader, training=True)
-        logger.info(f"Train loss = {train_loss['loss']}")
-        for key, value in train_loss.items():
-            writer.add_scalar(f"train/{key}", value, epoch)
-
-        # Test
-        if epoch % args.test_interval == 0:
-            test_loss = model.run(test_loader, training=False)
-            logger.info(f"Test loss = {test_loss['loss']}")
-            for key, value in test_loss.items():
-                writer.add_scalar(f"test/{key}", value, epoch)
-
-        # Sample data
-        if epoch % args.plot_interval == 0:
-            # Sample data
-            logger.info("Sample data")
-            z_sample, x_sample = model.sample(32)
-            writer.add_images(
-                "sample/latnet", z_sample.view(32, 1, -1, 1), epoch)
-            writer.add_images("sample/observable", x_sample, epoch)
-
-            # reconstruct data
-            logger.info("Reconstruct data")
-            z, x_recon = model.reconstruct(x_org[:8])
-            img = torch.cat([x_org[:8], x_recon])
-            writer.add_images("reconstruct/latent", z.view(8, 1, -1, 1), epoch)
-            writer.add_images("reconstruct/observable", img, epoch)
-
-        # Save model
-        if epoch % args.save_interval == 0:
-            logger.info(f"Save model at epoch {epoch}")
-            t = timer.strftime("%Y%m%d%H%M%S")
-            filename = f"{args.model}_{t}_epoch_{epoch}.pt"
-            torch.save({"distributions_dict": model.distributions.state_dict(),
-                        "optimizer_dict": model.optimizer.state_dict()},
-                       pathlib.Path(args.logdir, filename))
-
-    # -------------------------------------------------------------------------
-    # 5. Summary
-    # -------------------------------------------------------------------------
-
-    # Log hyper-parameters
-    hparam_dict = vars(args)
-    hparam_dict.update(config[f"{args.model}_params"])
-    metric_dict = {"summary/train_loss": train_loss["loss"]}
-    if "test_loss" in locals():
-        metric_dict.update({"summary/test_loss": test_loss["loss"]})
-    writer.add_hparams(hparam_dict, metric_dict)
-
-    writer.close()
+    # Trainer
+    trainer_params = {
+        # "logger": logger,
+        "default_save_path": args.logdir,
+        "gpus": gpus,
+        "check_val_every_n_epoch": args.test_interval,
+        "max_epochs": args.epochs,
+    }
+    trainer = pl.Trainer(**trainer_params)
+    trainer.fit(updater)
 
 
 def init_args():
     parser = argparse.ArgumentParser(description="VAE training")
-    parser.add_argument("--logdir", type=str, default="../logs/tmp/")
+    parser.add_argument("--logdir", type=str, default="../logs/")
     parser.add_argument("--root", type=str, default="../data/mnist/")
     parser.add_argument("--config", type=str, default="./config.json")
-    parser.add_argument("--model", type=str, default="vae")
-    parser.add_argument("--cuda-num", type=int, default=0)
+    parser.add_argument("--model", type=str, default="beta")
+    parser.add_argument("--cuda", type=str, default="0")
     parser.add_argument("--seed", type=int, default=0)
     parser.add_argument("--batch-size", type=int, default=128)
     parser.add_argument("--epochs", type=int, default=5)
@@ -153,30 +97,6 @@ def init_args():
     parser.add_argument("--save-interval", type=int, default=100)
 
     return parser.parse_args()
-
-
-def main():
-    # Args
-    args = init_args()
-
-    # Make logdir
-    dvu.check_logdir(args.logdir)
-
-    # Logger
-    logger = dvu.init_logger(args.logdir)
-    logger.info("Start logger")
-    logger.info(f"Command line args: {args}")
-
-    # Config
-    config = dvu.load_config(args.config)
-    logger.info(f"Configs: {config}")
-
-    try:
-        train(args, logger, config)
-    except Exception as e:
-        logger.exception(f"Run function error: {e}")
-    finally:
-        logger.info("End logger")
 
 
 if __name__ == "__main__":
