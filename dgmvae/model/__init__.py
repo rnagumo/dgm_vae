1229ba9aeb828f270755d0bf24b76c89fa2fef89
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-03-06 16:37:41 +0900

    Remove unused models

--- dgmvae/model/__init__.py
+++ dgmvae/model/__init__.py
@@ -1,9 +1,9 @@
 
 from .base import BaseVAE
 from .betavae import BetaVAE
-from .tcvae import TCVAE
-from .factorvae import FactorVAE
-from .dipvae import DIPVAE
-from .jointvae import JointVAE
-from .aae import AAE
-from .avb import AVB
+# from .tcvae import TCVAE
+# from .factorvae import FactorVAE
+# from .dipvae import DIPVAE
+# from .jointvae import JointVAE
+# from .aae import AAE
+# from .avb import AVB
