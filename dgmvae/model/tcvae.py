7aa89baecd57daa20297093839682bd0fa377245
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-03-07 16:56:25 +0900

    Update TC-VAE

--- dgmvae/model/tcvae.py
+++ dgmvae/model/tcvae.py
@@ -9,21 +9,77 @@ https://github.com/rtqichen/beta-tcvae
 """
 
 import math
+
 import torch
+from torch import nn
+
+import pixyz.distributions as pxd
+import pixyz.losses as pxl
 
 from .base import BaseVAE
+from .dist import Decoder, Encoder
 
 
 class TCVAE(BaseVAE):
-    def __init__(self, channel_num, z_dim, device, alpha, beta, gamma,
-                 **kwargs):
-        super().__init__(channel_num, z_dim, device, **kwargs)
+    def __init__(self, channel_num, z_dim, alpha, beta, gamma, **kwargs):
+        super().__init__()
 
-        self.alpha = alpha
-        self.beta = beta
-        self.gamma = gamma
+        # Parameters
+        self.channel_num = channel_num
+        self.z_dim = z_dim
 
-    def _eval_loss(self, x_dict, **kwargs):
+        self._alpha_value = alpha
+        self._beta_value = beta
+        self._gamma_value = gamma
+
+        # Distributions
+        self.prior = pxd.Normal(loc=torch.tensor(0.), scale=torch.tensor(1.),
+                                var=["z"], features_shape=[z_dim])
+        self.decoder = Decoder(channel_num, z_dim)
+        self.encoder = Encoder(channel_num, z_dim)
+        self.distributions = nn.ModuleList(
+            [self.prior, self.decoder, self.encoder])
+
+        # Loss class
+        self.ce = pxl.CrossEntropy(self.encoder, self.decoder)
+        self.kl = pxl.KullbackLeibler(self.encoder, self.prior)
+        self.alpha = pxl.Parameter("alpha")
+        self.beta = pxl.Parameter("beta")
+        self.gamma = pxl.Parameter("gamma")
+
+    def encode(self, x, mean=False):
+        if not isinstance(x, dict):
+            x = {"x": x}
+
+        if mean:
+            return self.encoder.sample_mean(x)
+        return self.encoder.sample(x, return_all=False)
+
+    def decode(self, z, mean=False):
+        if not isinstance(z, dict):
+            z = {"z": z}
+
+        if mean:
+            return self.decoder.sample_mean(z)
+        return self.decoder.sample(z, return_all=False)
+
+    def sample(self, batch_n=1):
+        z = self.prior.sample(batch_n=batch_n)
+        sample = self.decoder.sample_mean(z)
+        return sample
+
+    def forward(self, x, return_latent=False):
+        z = self.encode(x)
+        sample = self.decode(z, mean=True)
+        if return_latent:
+            z.update({"x": sample})
+            return z
+        return sample
+
+    def loss_func(self, x_dict, **kwargs):
+
+        x_dict.update({"alpha": self._alpha_value, "beta": self._beta_value,
+                       "gamma": self._gamma_value})
 
         # Sample z from encoder
         x_dict = self.encoder.sample(x_dict)
@@ -63,4 +119,13 @@ class TCVAE(BaseVAE):
         loss_dict = {"loss": loss, "recon": recon, "mutual_info": mutual_info,
                      "independence": independence, "dim_wise_kl": dim_wise_kl}
 
-        return loss, loss_dict
+        return loss_dict
+
+    @property
+    def loss_cls(self):
+        p_text = ("\\mathbb{E}_{q(z|n)p(n)} \\left[\\log p(n|z)\\right] "
+                  "- \\alpha I_q(z;n) "
+                  "- \\beta D_{KL}\\left[q(z) || \\prod_j q(z_j) \\right] "
+                  "- \\gamma \\sum_j D_{KL} \\left[q(z_j) || p(z_j) \\right]")
+
+        return p_text
