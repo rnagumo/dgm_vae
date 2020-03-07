b60f8ee9a557c7b46875f89c4e7dccb8c3c3e274
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-03-07 10:26:09 +0900

    Update FactorVAE

--- dgmvae/model/factorvae.py
+++ dgmvae/model/factorvae.py
@@ -14,33 +14,7 @@ import pixyz.distributions as pxd
 import pixyz.losses as pxl
 
 from .base import BaseVAE
-
-
-class Discriminator(pxd.Deterministic):
-    def __init__(self, z_dim):
-        super().__init__(cond_var=["z"], var=["t"])
-
-        self.model = nn.Sequential(
-            nn.Linear(z_dim, 1000),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(1000, 1000),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(1000, 1000),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(1000, 1000),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(1000, 1000),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(1000, 1000),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(1000, 1),
-        )
-
-    def forward(self, z):
-        logits = self.model(z)
-        probs = torch.sigmoid(logits)
-        t = torch.clamp(probs, 1e-6, 1 - 1e-6)
-        return {"t": t}
+from .dist import Decoder, Encoder, Discriminator
 
 
 class InferenceShuffleDim(pxd.Deterministic):
@@ -55,63 +29,92 @@ class InferenceShuffleDim(pxd.Deterministic):
 
 
 class FactorVAE(BaseVAE):
-    def __init__(self, channel_num, z_dim, device, beta, gamma, **kwargs):
-        super().__init__(channel_num, z_dim, device, beta, **kwargs)
+    def __init__(self, channel_num, z_dim, beta, gamma, **kwargs):
+        super().__init__()
 
-        self.gamma = gamma
+        # Parameters
+        self.channel_num = channel_num
+        self.z_dim = z_dim
+        self._beta_value = beta
+        self._gamma_value = gamma
 
         # Dimension shuffle
-        self.encoder_shf = InferenceShuffleDim(self.encoder).to(device)
-
-        # Discriminator
-        self.disc = Discriminator(z_dim).to(device)
+        self.prior = pxd.Normal(loc=torch.tensor(0.), scale=torch.tensor(1.),
+                                var=["z"], features_shape=[z_dim])
+        self.decoder = Decoder(channel_num, z_dim)
+        self.encoder = Encoder(channel_num, z_dim)
+        self.encoder_shf = InferenceShuffleDim(self.encoder)
+        self.distributions = nn.ModuleList(
+            [self.prior, self.decoder, self.encoder, self.encoder_shf])
 
         # Loss
-        self.tc = pxl.AdversarialKullbackLeibler(
+        self.ce = pxl.CrossEntropy(self.encoder, self.decoder)
+        self.kl = pxl.KullbackLeibler(self.encoder, self.prior)
+        self.beta = pxl.Parameter("beta")
+        self.gamma = pxl.Parameter("gamma")
+
+        # Adversarial loss (Total Correlation)
+        self.disc = Discriminator(z_dim)
+        self.adv_js = pxl.AdversarialKullbackLeibler(
             self.encoder, self.encoder_shf, self.disc)
 
-    def _eval_loss(self, x_dict, **kwargs):
+    def encode(self, x, mean=False):
+        if not isinstance(x, dict):
+            x = {"x": x}
 
-        ce_loss = self.ce.eval(x_dict).mean()
-        kl_loss = self.beta * self.kl.eval(x_dict).mean()
-        tc_loss = self.gamma * self.tc.eval(x_dict)
+        if mean:
+            return self.encoder.sample_mean(x)
+        return self.encoder.sample(x, return_all=False)
 
-        loss = ce_loss + kl_loss + tc_loss
-        loss_dict = {"loss": loss.item(), "ce_loss": ce_loss.item(),
-                     "kl_loss": kl_loss.item(), "tc_loss": tc_loss.item()}
+    def decode(self, z, mean=False):
+        if not isinstance(z, dict):
+            z = {"z": z}
 
-        return loss, loss_dict
+        if mean:
+            return self.decoder.sample_mean(z)
+        return self.decoder.sample(z, return_all=False)
 
-    def run(self, loader, training=True):
-        """Overrides super().run() because tc needs to be trained."""
+    def sample(self, batch_n=1):
+        z = self.prior.sample(batch_n=batch_n)
+        sample = self.decoder.sample_mean(z)
+        return sample
 
-        # Returned value
-        loss_dict = collections.defaultdict(float)
+    def forward(self, x, return_latent=False):
+        z = self.encode(x)
+        sample = self.decode(z, mean=True)
+        if return_latent:
+            z.update({"x": sample})
+            return z
+        return sample
 
-        for x in loader:
-            if isinstance(x, (tuple, list)):
-                x = x[0]
-            x = x.to(self.device)
-            len_x = x.size(0) // 2
+    def loss_func(self, x_dict, **kwargs):
 
-            # Mini-batch size
-            minibatch_size = x.size(0)
+        optimizer_idx = kwargs["optimizer_idx"]
 
-            # Calculate loss
-            if training:
-                _batch_loss = self.train({"x": x[:len_x], "x_shf": x[len_x:]})
-                _d_loss = self.tc.train({"x": x[:len_x], "x_shf": x[len_x:]})
-            else:
-                _batch_loss = self.test({"x": x[:len_x], "x_shf": x[len_x:]})
-                _d_loss = self.tc.test({"x": x[:len_x], "x_shf": x[len_x:]})
+        x = x_dict["x"]
+        len_x = x.size(0)
+        x_dict.update({"x": x[:len_x], "x_shf": x[len_x:]})
+        x_dict.update({"beta": self._beta_value, "gamma": self._gamma_value})
 
-            # Accumulate minibatch loss
-            for key in _batch_loss:
-                loss_dict[key] += _batch_loss[key] * minibatch_size
-            loss_dict["d_loss"] += _d_loss.item() * minibatch_size
+        if optimizer_idx == 0:
+            # VAE loss
+            ce_loss = self.ce.eval(x_dict).mean()
+            kl_loss = (self.beta * self.kl).eval(x_dict).mean()
+            tc_loss = (self.gamma * self.adv_js).eval(x_dict)
+            loss = ce_loss + kl_loss + tc_loss
 
-        # Devide by data size
-        for key in loss_dict:
-            loss_dict[key] /= len(loader.dataset)
+            loss_dict = {"loss": loss, "ce_loss": ce_loss, "kl_loss": kl_loss,
+                         "tc_loss": tc_loss}
+            return loss_dict
+        elif optimizer_idx == 1:
+            # Discriminator loss
+            loss = self.adv_js.eval(x_dict, discriminator=True)
+            return {"adv_loss": loss}
 
-        return loss_dict
+    @property
+    def loss_cls(self):
+        return self.ce + self.beta * self.kl + self.gamma * self.adv_js
+
+    @property
+    def second_optim(self):
+        return self.adv_js.d_optimizer
