f3c0714614a31c77d7c9512631e421076051cfdf
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-03-08 10:19:49 +0900

    Update required packages

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 
 
 install_requires = [
-    "pytorch-lightning>=0.7.0",
     "torch>=1.4.0",
     "torchvision>=0.5.0",
+    "pytorch-lightning>=0.7.0",
     "pixyz>=0.1.4",
 ]
 
