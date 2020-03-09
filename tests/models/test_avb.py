de010654c7f4765c5643dc1e882fe987fe9e9de4
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-03-09 19:52:42 +0900

    Add test case of AVB

---
+++ tests/models/test_avb.py
@@ -0,0 +1,93 @@
+
+import unittest
+
+import torch
+
+import dgmvae.models as dgm
+
+
+class TestAVB(unittest.TestCase):
+
+    def setUp(self):
+        self.batch_n = 5
+        self.z_dim = 2
+        self.e_dim = 2
+
+        params = {
+            "channel_num": 1,
+            "z_dim": self.z_dim,
+            "e_dim": self.e_dim,
+            "beta": 1
+        }
+        self.model = dgm.AVB(**params)
+
+    def test_encode(self):
+        x = torch.randn(self.batch_n, 1, 64, 64)
+
+        latent = self.model.encode(x)
+        self.assertIsInstance(latent, dict)
+        self.assertEqual(
+            latent["z"].size(), torch.Size([self.batch_n, self.z_dim]))
+
+        z = self.model.encode(x, mean=True)
+        self.assertEqual(z.size(), torch.Size([self.batch_n, self.z_dim]))
+
+    def test_decode(self):
+        latent = torch.randn(self.batch_n, self.z_dim)
+
+        obs = self.model.decode(latent)
+        self.assertIsInstance(obs, dict)
+        self.assertEqual(
+            obs["x"].size(), torch.Size([self.batch_n, 1, 64, 64]))
+
+        obs = self.model.decode(latent, mean=True)
+        self.assertIsInstance(obs, torch.Tensor)
+        self.assertEqual(obs.size(), torch.Size([self.batch_n, 1, 64, 64]))
+
+    def test_sample(self):
+        batch_n = 2
+        obs = self.model.sample(batch_n=batch_n)
+
+        self.assertIsInstance(obs, torch.Tensor)
+        self.assertEqual(obs.size(), torch.Size([batch_n, 1, 64, 64]))
+
+    def test_forward(self):
+        x = torch.randn(self.batch_n, 1, 64, 64)
+
+        # 1. default
+        z = self.model(x)
+        self.assertEqual(z.size(), torch.Size([self.batch_n, self.z_dim]))
+
+        # 2. reconstruct without latent
+        obs = self.model(x, reconstruct=True)
+        self.assertIsInstance(obs, torch.Tensor)
+        self.assertEqual(obs.size(), torch.Size([self.batch_n, 1, 64, 64]))
+
+        # 3. reconstruct with latent
+        sample = self.model(x, reconstruct=True, return_latent=True)
+        self.assertIsInstance(sample, dict)
+        self.assertEqual(
+            sample["x"].size(), torch.Size([self.batch_n, 1, 64, 64]))
+        self.assertEqual(
+            sample["z"].size(), torch.Size([self.batch_n, self.z_dim]))
+
+    def test_loss_func(self):
+        x = torch.randn(self.batch_n, 1, 64, 64)
+
+        loss_dict = self.model.loss_func(x, optimizer_idx=0)
+        self.assertTrue(loss_dict["loss"] >= 0)
+        self.assertTrue(loss_dict["ce_loss"] >= 0)
+        self.assertTrue(loss_dict["js_loss"] >= 0)
+
+        adv_dict = self.model.loss_func(x, optimizer_idx=1)
+        self.assertTrue(adv_dict["adv_loss"] >= 0)
+
+    def test_loss_str(self):
+        self.assertIsInstance(self.model.loss_str, str)
+
+    def test_second_optim(self):
+        self.assertIsInstance(self.model.second_optim, torch.optim.Adam)
+
+
+if __name__ == "__main__":
+    unittest.main()
