d4131bef45d2dcc8802fabc89de62db7c9d4434b
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-03-10 11:00:10 +0900

    Add training bash

--- bin/train.sh
+++ bin/train.sh
@@ -1,2 +1,8 @@
 
-python ./src/train.py
+export CONFIG_PATH=./src/config.json
+export OUTPUT_PATH=./logs/
+export DATA_ROOT=./data/mnist/
+export DATASET_NAME=mnist
+export MODEL_NAME=beta
+export EVALUATION_NAME=${MODEL_NAME}/${DATASET_NAME}
+python ./src/train.py --model ${MODEL_NAME} --epochs 1
