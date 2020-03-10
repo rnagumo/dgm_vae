9e9283b017ed8babf7209467bfbc44ba29d2455e
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-03-10 11:07:37 +0900

    Update train.sh

--- bin/train.sh
+++ bin/train.sh
@@ -1,8 +1,8 @@
 
 export CONFIG_PATH=./src/config.json
 export OUTPUT_PATH=./logs/
-export DATA_ROOT=./data/mnist/
 export DATASET_NAME=mnist
+export DATA_ROOT=./data/${DATASET_NAME}/
 export MODEL_NAME=beta
 export EVALUATION_NAME=${MODEL_NAME}/${DATASET_NAME}
 python ./src/train.py --model ${MODEL_NAME} --epochs 1
