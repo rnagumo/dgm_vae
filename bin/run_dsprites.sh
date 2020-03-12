b1890d770e632681417bb5bae7b0a82b4ac0344e
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-03-12 22:39:49 +0900

    Merge train and eval method to run

--- bin/run_dsprites.sh
+++ bin/run_dsprites.sh
@@ -7,14 +7,15 @@ SEED=${2:-0}
 
 # Dataset path
 export DISENTANGLEMENT_LIB_DATA=./data/
-export DATASET_NAME=cars3d
-export DATA_ROOT=./${DISENTANGLEMENT_LIB_DATA}/cars/
+export DATASET_NAME=dsprites
+export DATA_ROOT=./${DISENTANGLEMENT_LIB_DATA}/${DATASET_NAME}/
 
 # Logging path
 export OUTPUT_PATH=./logs/
 export EVALUATION_NAME=${DATASET_NAME}_${MODEL_NAME}_${SEED}/
 
 # Config for training
-export CONFIG_PATH=./src/config_ch3.json
+export CONFIG_PATH=./src/config_ch1.json
 
 python ./src/train.py --model ${MODEL_NAME} --epochs 100 --seed ${SEED}
+python ./src/local_evaluation.py
