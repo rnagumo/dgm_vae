8a4ad05def8c447af1f42fc885f432531dbe81b8
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-03-12 20:50:37 +0900

    Fix dataset name and dataset directory of cars3d

--- bin/eval_cars3d.sh
+++ bin/eval_cars3d.sh
@@ -7,8 +7,8 @@ SEED=${2:-0}
 
 # Dataset path
 export DISENTANGLEMENT_LIB_DATA=./data/
-export DATASET_NAME=cars
-export DATA_ROOT=./${DISENTANGLEMENT_LIB_DATA}/${DATASET_NAME}/
+export DATASET_NAME=cars3d
+export DATA_ROOT=./${DISENTANGLEMENT_LIB_DATA}/cars/
 
 # Logging path
 export OUTPUT_PATH=./logs/
