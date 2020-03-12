5e774e385c9ebb57d9923546639274bc455415ca
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-03-12 20:43:34 +0900

    Fix dataset path in test case

--- tests/datasets/test_cars3d.py
+++ tests/datasets/test_cars3d.py
@@ -8,7 +8,7 @@ class TestCars3dDataset(unittest.TestCase):
 
     def test_dataset(self):
         # Need pre-downloaded dataset
-        dataset = dvd.Cars3dDataset("../../data/cars3d/")
+        dataset = dvd.Cars3dDataset("../../data/cars/")
 
         x, y = dataset[0]
         self.assertTupleEqual(x.size(), (3, 64, 64))
