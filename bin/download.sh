52d873202e77388ac814595a5657c5bd11ea379e
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-03-12 19:30:02 +0900

    Update download.sh

--- bin/download.sh
+++ bin/download.sh
@@ -4,24 +4,27 @@
 export DISENTANGLEMENT_LIB_DATA=./data/
 cd ${DISENTANGLEMENT_LIB_DATA}
 
-echo "Downloading dSprites dataset."
+echo "Check dSprites"
 if [[ ! -d "dsprites" ]]; then
+  echo "Downloading dSprites dataset."
   mkdir dsprites
   wget -O dsprites/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz https://github.com/deepmind/dsprites-dataset/raw/master/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz
+  echo "Downloading dSprites completed!"
 fi
-echo "Downloading dSprites completed!"
 
-# echo "Downloading mpi3d_toy dataset."
+echo "Check mpi3d_toy"
 # if [[ ! -d "mpi3d_toy" ]]; then
+#   echo "Downloading mpi3d_toy dataset."
 #   mkdir mpi3d_toy
 #   wget -O mpi3d_toy/mpi3d_toy.npz https://storage.googleapis.com/disentanglement_dataset/data_npz/sim_toy_64x_ordered_without_heldout_factors.npz
+#   echo "Downloading mpi3d_toy completed!"
 # fi
-# echo "Downloading mpi3d_toy completed!"
 
-echo "Downloading cars3d dataset."
+echo "Check cars3d"
 if [[ ! -d "cars3d" ]]; then
+  echo "Downloading cars3d dataset."
   mkdir cars3d
   wget -O cars3d/cars3d.tar.gz http://www.scottreed.info/files/nips2015-analogy-data.tar.gz
-  tar -zxvf cars3d/cars3d.tar.gz
+  tar -zxf cars3d/cars3d.tar.gz cars3d/
+  echo "Downloading cars3d completed!"
 fi
-echo "Downloading cars3d completed!"
