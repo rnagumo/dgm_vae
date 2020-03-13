93c0a895c5ddae66d6d72fe9c160fcab9d6da21b
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-03-13 19:27:21 +0900

    Fix dSprites dataset class to save memory

--- dgmvae/datasets/dsprites.py
+++ dgmvae/datasets/dsprites.py
@@ -20,18 +20,16 @@ class DSpritesDataset(torch.utils.data.Dataset):
         filename = "dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz" \
             if filename is None else filename
         path = pathlib.Path(root, filename)
-        with open(path, "rb") as f:
-            dataset = np.load(f, encoding="latin1", allow_pickle=True)
-            data = torch.tensor(
-                dataset["imgs"], dtype=torch.float32).unsqueeze(1)
+        with np.load(path, encoding="latin1", allow_pickle=True) as dataset:
+            data = torch.tensor(dataset["imgs"])
             targets = torch.tensor(dataset["latents_classes"])
 
-        # Reshape dataset (batch, channel, height, width)
         self.data = data
         self.targets = targets
 
     def __getitem__(self, index):
-        return self.data[index], self.targets[index]
+        # Reshape dataset (channel, height, width)
+        return self.data[index].unsqueeze(0).float(), self.targets[index]
 
     def __len__(self):
         return self.data.size(0)
