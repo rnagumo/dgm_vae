e23250c4fc24da765ee80276bfbfaf23f5c9c1b3
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-04-08 16:38:35 +0900

    Fix factor vae metric

--- dgmvae/metrics/factor_vae_metric.py
+++ dgmvae/metrics/factor_vae_metric.py
@@ -11,12 +11,13 @@ import numpy as np
 
 
 def factor_vae_metric(dataset, repr_fn, batch_size=64, num_train=10000,
-                      num_eval=5000, num_var=10000):
+                      num_eval=5000, num_var=10000, th_var=0.05):
     """Computes factor-VAE metric."""
 
     # Compute global variance
     global_var = _compute_variance(dataset, repr_fn, num_var)
-    if all(global_var == 0):
+    active_dims = (global_var ** 2) > th_var
+    if all(~active_dims):
         scores = {
             "train_accuracy": 0.0,
             "eval_accuracy": 0.0,
@@ -26,9 +27,9 @@ def factor_vae_metric(dataset, repr_fn, batch_size=64, num_train=10000,
 
     # Sample data
     votes_train = _generate_batch(
-        dataset, repr_fn, batch_size, num_train, global_var)
+        dataset, repr_fn, batch_size, num_train, global_var, active_dims)
     votes_eval = _generate_batch(
-        dataset, repr_fn, batch_size, num_eval, global_var)
+        dataset, repr_fn, batch_size, num_eval, global_var, active_dims)
 
     # Majority-vote classifier
     classifier = np.argmax(votes_train, axis=0)
@@ -46,23 +47,28 @@ def factor_vae_metric(dataset, repr_fn, batch_size=64, num_train=10000,
 
 
 def _compute_variance(dataset, repr_fn, num_var):
+    """Computes variances for each dimension of the representation."""
+
     data, _ = dataset.sample_batch(num_var)
     reprs = repr_fn(data)
     return reprs.var(0)
 
 
-def _generate_batch(dataset, repr_fn, batch_size, num_points, global_var):
+def _generate_batch(dataset, repr_fn, batch_size, num_points, global_var,
+                    active_dims):
 
     votes = np.zeros((dataset.num_factors, global_var.shape[0]))
     for _ in range(num_points):
         factor_index, argmin = _generate_sample(
-            dataset, repr_fn, batch_size, global_var)
-        votes[factor_index, argmin]
+            dataset, repr_fn, batch_size, global_var, active_dims)
+
+        # Count (factor index, minimum variance latents) pairs
+        votes[factor_index, argmin] += 1
 
     return votes
 
 
-def _generate_sample(dataset, repr_fn, batch_size, global_var):
+def _generate_sample(dataset, repr_fn, batch_size, global_var, active_dims):
 
     # Select random coordinate to keep fixed
     factor_index = dataset.sample_factor_index()
@@ -75,7 +81,6 @@ def _generate_sample(dataset, repr_fn, batch_size, global_var):
 
     # Compute local variance
     local_var = reprs.var(0)
-    active_dims = global_var > 0
     argmin = np.argmin(local_var[active_dims] / global_var[active_dims])
 
     return factor_index, argmin
