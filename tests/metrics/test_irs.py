b26febbe10b4423a310fc4be37642aba7561eadb
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-04-08 18:51:20 +0900

    Fix kwargs of irs

--- tests/metrics/test_irs.py
+++ tests/metrics/test_irs.py
@@ -7,6 +7,6 @@ from .shared import TestMetricBase
 class TestIRSMetric(TestMetricBase):
 
     def test_metric(self):
-        scores = dgm.irs(self.dataset, self.repr_fn, num_train=100)
+        scores = dgm.irs(self.dataset, self.repr_fn, num_points=100)
         self.assertTrue(0.0 < scores["irs"] < 1.0)
         self.assertGreaterEqual(scores["num_active_dims"], 0)
