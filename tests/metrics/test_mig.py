d1730d3a651ce2603862724ce820f0445f3d2800
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-04-09 09:34:36 +0900

    Fix kwargs in mig

--- tests/metrics/test_mig.py
+++ tests/metrics/test_mig.py
@@ -7,5 +7,5 @@ from .shared import TestMetricBase
 class TestMIG(TestMetricBase):
 
     def test_metric(self):
-        scores = dgm.mig(self.dataset, self.repr_fn, num_train=100)
+        scores = dgm.mig(self.dataset, self.repr_fn, num_points=100)
         self.assertTrue(0 <= scores["discrete_mig"] <= 1)
