a1245ca987ae28de78cae0280b01a0caf5002e83
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-05-14 10:30:01 +0900

    Remove experiment from library

--- src/train.py
+++ src/train.py
@@ -12,9 +12,8 @@ from torch.backends import cudnn
 import pytorch_lightning as pl
 
 import dgmvae.models as dvm
-import dgmvae.updaters as dvu
 
-import utils_pytorch as utils
+from experiment import VAEUpdater
 
 
 def main():
@@ -27,14 +26,15 @@ def main():
     args = init_args()
 
     # Configs
-    condig_path = os.getenv("CONFIG_PATH", "./src/config.json")
+    condig_path = os.getenv("CONFIG_PATH", "./src/config_ch1.json")
     with pathlib.Path(condig_path).open() as f:
         config = json.load(f)
 
     # Path
-    root = os.getenv("DATA_ROOT", "./data/mnist/")
+    root = pathlib.Path(os.getenv("DATA_ROOT", "./data/mnist/"))
     save_path = pathlib.Path(os.getenv("SAVE_PATH", "./logs/"),
                              os.getenv("EVALUATION_NAME", "dev"))
+    model_path = save_path / "representation"
     dataset = os.getenv("DATASET_NAME", "mnist")
 
     # Cuda setting
@@ -65,7 +65,7 @@ def main():
     model = model_dict[args.model](**config[f"{args.model}_params"])
 
     # Updater
-    updater = dvu.VAEUpdater(model, args, dataset, root, args.batch_size)
+    updater = VAEUpdater(model, args, dataset, root, args.batch_size)
 
     # Trainer
     params = {
@@ -81,8 +81,28 @@ def main():
     trainer.fit(updater)
 
     # Export model
+    model_path.mkdir()
     ch_num = config[f"{args.model}_params"]["channel_num"]
-    utils.export_model(updater.model, input_shape=(1, ch_num, 64, 64))
+    export_model(updater.model, str(model_path / "pytorch_model.pt"),
+                 input_shape=(1, ch_num, 64, 64))
+
+
+def export_model(model, path, input_shape=(1, 3, 64, 64),
+                 use_script_module=True):
+    model = model.cpu().eval()
+    if isinstance(model, torch.jit.ScriptModule):
+        assert use_script_module, \
+            "Provided model is a ScriptModule, set use_script_module to True."
+    if use_script_module:
+        if not isinstance(model, torch.jit.ScriptModule):
+            assert input_shape is not None
+            traced_model = torch.jit.trace(model, torch.zeros(*input_shape))
+        else:
+            traced_model = model
+        torch.jit.save(traced_model, path)
+    else:
+        torch.save(model, path)  # saves model as a nn.Module
+    return path
 
 
 def init_args():
