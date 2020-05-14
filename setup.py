084dbcb77c692aa40ae9163f64b36b1105e402d9
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-05-14 16:25:05 +0900

    Update dependencies and library version

--- setup.py
+++ setup.py
@@ -5,20 +5,15 @@ from setuptools import setup, find_packages
 install_requires = [
     "torch==1.4.0",
     "torchvision==0.5.0",
-    "pytorch-lightning==0.7.1",
     "pixyz==0.1.4",
-    "disentanglement-lib==1.4",
     "numpy==1.18.1",
-    "tensorflow==1.14.0",
-    "tensorflow-probability==0.7.0",
     "scikit-learn==0.22.2",
-    "pandas==1.0.1",
 ]
 
 
 setup(
     name="dgmvae",
-    version="0.4",
+    version="0.5",
     packages=find_packages(),
     install_requires=install_requires,
 )
